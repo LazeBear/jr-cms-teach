4ec515d0faa5e88ffb29b69df2070c9f76996347
Mason Xiong <mason.xiong@yahoo.com>
2019-05-27 17:10:23 +1000

    add student controller

---
+++ src/controllers/student.js
@@ -0,0 +1,17 @@
+function addStudent(req, res) {}
+
+function getStudent(req, res) {}
+
+function getAllStudents(req, res) {}
+
+function updateStudent(req, res) {}
+
+function deleteStudent(req, res) {}
+
+module.exports = {
+  addStudent,
+  getAllStudents,
+  getStudent,
+  updateStudent,
+  deleteStudent
+};
