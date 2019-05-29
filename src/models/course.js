699b653db4cce20e82f49b53309714bf81675bcc
Mason Xiong <mason.xiong@yahoo.com>
2019-05-29 11:54:04 +1000

    understand virtual property and how to hide some properties or timestamps

--- src/models/course.js
+++ src/models/course.js
@@ -1,18 +1,32 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-  _id: {
-    type: String,
-    uppercase: true
+const schema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      uppercase: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    __v: { type: Number, select: false }
   },
-  name: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    default: ''
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true
+    },
+    id: false
   }
+);
+
+schema.virtual('code').get(function() {
+  return this._id;
 });
 
 const model = mongoose.model('Course', schema);
