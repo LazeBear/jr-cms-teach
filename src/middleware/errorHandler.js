3347db62da364ea0d337a44b8363673465099e6d
Mason Xiong <mason.xiong@yahoo.com>
2019-05-29 15:19:38 +1000

    log in error handler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -2,5 +2,6 @@ module.exports = (err, req, res, next) => {
   if (err.name === 'ValidationError') {
     return res.status(400).json(err.message);
   }
+  console.error(err);
   return res.status(500).json('something unexpected happened');
 };
